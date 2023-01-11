2c1edaacf052b38208f06a74c3309354a7d61c4b
Yetunde Alese <yaalese1@gmail.com>
2023-01-11 13:57:45 -0500

    commit tree f20f34b52a7682a86d7d6810a7d980b98a8506dc

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true ,uniqueness: true #but when in the console i added the same author name 
+    validates :phone_number , length:{is:10}
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+ validates :title, presence: true
+ validates :content, length: {minimum: 250}
+ validates :summary, length: {minimum: 250 ,maximum: 250 }
+ validates :category, inclusion: { in: ['Fiction', or 'Non-Fiction'] }
+  
+     validate :clickbait 
+
+       def clickbait
+         unless title.match?("Won't Believe", "Secret","Top [number]", "guess" )
+          errors.add(:clickbait "must contain...")
+      end  
+   end
 end
+
+  
